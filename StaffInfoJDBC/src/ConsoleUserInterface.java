227e5c2701d681987e28f8a97fcdd8f1e5b6a1e1
Sambit Senapati <sambaapu@gmail.com>
2023-12-06 03:05:19 -0500

    added more input validation

--- StaffInfoJDBC/src/ConsoleUserInterface.java
+++ StaffInfoJDBC/src/ConsoleUserInterface.java
@@ -47,9 +47,9 @@ public class ConsoleUserInterface extends UserInterface {
                 id = scanner.next();
                 System.out.print("Enter column name to update: ");
                 String col = scanner.next();
-                if(isColumn(col)){
-                    System.out.print("Enter column value to update: ");
-                    String colVal = scanner.next();
+                System.out.print("Enter column value to update: ");
+                String colVal = scanner.next();
+                if(updateDataValidation(col,colVal)){
                     staffRepository.updateStaff(col, colVal, id);
                 }
                 break;
@@ -79,19 +79,27 @@ public class ConsoleUserInterface extends UserInterface {
 
         System.out.print("ID: ");
         String id = scanner.next();
-        if (!isInteger(id)) {
-            System.out.println("Invalid address format. Please enter a valid address.");
+        if (!validateID(id)) {
             return null;
         }
 
         System.out.print("Last Name: ");
         String lastName = scanner.next();
+        if (!validateName(lastName)) {
+            return null;
+        }
 
         System.out.print("First Name: ");
         String firstName = scanner.next();
+        if (!validateName(firstName)) {
+            return null;
+        }
 
         System.out.print("MI: ");
-        char mi = scanner.next().charAt(0);
+        String mi = scanner.next();
+        if (!validateMi(mi)) {
+            return null;
+        }
 
         System.out.print("Age: ");
         String ageStr = scanner.next();
@@ -103,16 +111,21 @@ public class ConsoleUserInterface extends UserInterface {
 
         System.out.print("Address: ");
         String address = scanner.next();
-        if (!isValidAddress(address)) {
-            System.out.println("Invalid address format. Please enter a valid address.");
+        if (!validateAddress(address)) {
             return null;
         }
 
         System.out.print("City: ");
         String city = scanner.next();
+        if (!validateCity(city)) {
+            return null;
+        }
 
         System.out.print("State: ");
         String state = scanner.next();
+        if (!validateState(state)) {
+            return null;
+        }
 
         System.out.print("Telephone: ");
         String telephone = scanner.next();
@@ -127,7 +140,7 @@ public class ConsoleUserInterface extends UserInterface {
             return null;
         }
 
-        Staff newStaff = new Staff(id, lastName, firstName, mi, age, address, city, state, telephone, email);
+        Staff newStaff = new Staff(id, lastName, firstName, mi.charAt(0), age, address, city, state, telephone, email);
         return newStaff;
     }
 
@@ -136,13 +149,6 @@ public class ConsoleUserInterface extends UserInterface {
         // Simple email validation using a regular expression
         String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
         return email.matches(emailRegex);
-    }
-    
-    private static boolean isValidAddress(String address) {
-        // Simple address validation using a regular expression
-        // You may need a more complex validation based on your requirements
-        String addressRegex = "^[a-zA-Z0-9\\s]+(?:\\s[a-zA-Z0-9]+)*$";
-        return address.matches(addressRegex);
     }
 
     private static boolean isValidPhoneNumber(String phoneNumber) {
@@ -160,7 +166,7 @@ public class ConsoleUserInterface extends UserInterface {
         }
         return true;
     }
-    public boolean isColumn(String column){
+    private boolean updateDataValidation(String column, String colVal){
         HashSet<String> columns = new HashSet<String>();
         columns.add("id");
         columns.add("lastName");
@@ -173,10 +179,108 @@ public class ConsoleUserInterface extends UserInterface {
         columns.add("telephone");
         columns.add("email");
         if (columns.contains(column)){
+            switch(column){
+                case "id":
+                    if (!isInteger(colVal)) {
+                        System.out.println("Invalid value for id. Enter integer value");
+                        return false;
+                    }
+                    break;
+                case "lastName":
+                    if (!validateName(colVal)) {
+                        return false;
+                    }
+                    break;
+                case "firstName":
+                    if (!validateName(colVal)) {
+                        return false;
+                    }
+                    break;
+                case "mi":
+                    if (!validateMi(colVal)) {
+                        return false;
+                    }
+                    break;
+                case "age":
+                    if (!isInteger(colVal)) {
+                        System.out.println("Invalid value for age. Enter integer value.");
+                        return false;
+                    }
+                    break;
+                case "address":
+                    if (!validateAddress(colVal)) {
+                        return false;
+                    }
+                    break;
+                case "city":
+                    if (!validateCity(colVal)) {
+                        return false;
+                    }
+                    break;
+                case "state":
+                    if (!validateState(colVal)) {
+                        return false;
+                    }
+                    break;
+                case "telephone":
+                    if (!isValidPhoneNumber(colVal)) {
+                        System.out.println("Invalid telephone number. Please enter a valid 10-digit phone number.");
+                        return false;
+                    }
+                    break;
+                case "email":
+                if (!isValidEmail(colVal)) {
+                        System.out.println("Invalid email address.");
+                        return false;
+                    }
+                    break;
+            }
             return true;
         }else{
             System.out.println("Column not found. Columns in Staff table are: " + columns);
             return false;
         }
+    }
+    private boolean validateID(String id){
+        if (!isInteger(id) || id.length() > 9) {
+            System.out.println("Invalid value for ID. Enter integer value and length should be less than 9 digit.");
+            return false;
+        }
+        return true;
+    }
+    private boolean validateName(String name){
+        if (!name.matches("[a-zA-Z]+") || name.length() > 15) {
+            System.out.println("Invalid value for Name. Enter string value and length should be less than 15 characters");
+            return false;
+        }
+        return true;
+    }
+    private boolean validateMi(String mi){
+        if ( !mi.matches("[a-zA-Z]") || mi.length() > 1) {
+            System.out.println("Invalid value for mi. Enter single alphabetic character");
+            return false;
+        }
+        return true;
+    }
+    private boolean validateAddress(String address){
+        if ( !address.matches("^[a-zA-Z0-9\\s]+$") || address.length() > 20) {
+            System.out.println("Invalid Address. Enter address with length 20 or less");
+            return false;
+        }
+        return true;
+    }
+    private boolean validateCity(String city){
+        if (!city.matches("[a-zA-Z]+") || city.length() > 15) {
+            System.out.println("Invalid value for City. Enter string value and length should be 20 characters or less");
+            return false;
+        }
+        return true;
+    }
+    private boolean validateState(String state){
+        if (!state.matches("[A-Z]+") || state.length() > 2) {
+            System.out.println("Invalid value for Name. Capital letters only. Length should be 2 characters or less");
+            return false;
+        }
+        return true;
     }
 }
